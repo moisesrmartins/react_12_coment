e9c0622d8af1bac914490c423c81e9785ebfed45
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-12-12 15:42:57 -0300

    React_12_Coment Parte 1: Firebase.js

---
+++ coment/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV5v1juHKPWeh_6NS3CI7Be9wQHa__wRU",
+  authDomain: "coment-b5982.firebaseapp.com",
+  projectId: "coment-b5982",
+  storageBucket: "coment-b5982.appspot.com",
+  messagingSenderId: "591752607648",
+  appId: "1:591752607648:web:ab5af9a001bb920aee96c5",
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
