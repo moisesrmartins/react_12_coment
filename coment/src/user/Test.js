5a70513a67d2f4584f741e634dde8fd9795af388
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-12-13 18:36:36 -0300

    React_12_Coment Parte 7: Test.js

---
+++ coment/src/user/Test.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import firebase from "../firebase";
+
+const useDatabase = (endpoint) => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const ref = firebase.database().ref(endpoint);
+    ref.on("value", (snapshot) => {
+      setData(snapshot.val());
+    });
+    return () => {
+      ref.off();
+    };
+  }, [endpoint]);
+  return data;
+};
+
+const Coment = ({ visible }) => {
+  const endpoint = visible ? "test" : "test/a";
+  const data = useDatabase(endpoint);
+  return <pre>{JSON.stringify(data)}</pre>;
+};
+
+const Coment2 = ({ visible }) => {
+  const endpoint = visible ? "test" : "test/b";
+  const data2 = useDatabase(endpoint);
+  return <pre>{JSON.stringify(data2)}</pre>;
+};
+
+const useDatabasePush = (endpoint) => {
+  const [status, setStatus] = useState("");
+
+  const save = (data) => {
+    const ref = firebase.database().ref(endpoint);
+    ref.push(data, (err) => {
+      if (err) {
+        setStatus("ERROR");
+      } else {
+        setStatus("SUCESS");
+      }
+    });
+  };
+
+  return [status, save];
+};
+
+const useDatabasePush2 = (endpoint) => {
+  const [status2, setStatus2] = useState("");
+
+  const save2 = (data) => {
+    const ref = firebase.database().ref(endpoint);
+    ref.push(data, (err) => {
+      if (err) {
+        setStatus2("ERROR");
+      } else {
+        setStatus2("SUCESS");
+      }
+    });
+  };
+
+  return [status2, save2];
+};
+
+function Test() {
+  const [visible, toggle] = useState(false);
+  const [status, save] = useDatabasePush("test/status/status1");
+
+  const [visible2, toggle2] = useState(false);
+  const [status2, save2] = useDatabasePush2("test/status/status2");
+
+  return (
+    <div>
+      <div>
+        <p>Status:{status}</p>
+        <button
+          onClick={() => {
+            toggle(!visible);
+            save({ a: 1 });
+          }}
+        >
+          Test/a
+        </button>
+        {visible && <Coment />}
+      </div>
+      <div>
+        <p>Status:{status2}</p>
+        <button
+          onClick={() => {
+            toggle2(!visible2);
+            save2({ b: 2 });
+          }}
+        >
+          Test/b
+        </button>
+        {visible2 && <Coment2 />}
+      </div>
+    </div>
+  );
+}
+
+export default Test;
