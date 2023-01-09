a3616c903967bb60c3b996cff9610b526e804ea3
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2023-01-09 17:28:44 -0300

    React_12_Coment Parte 21: Auth.js

--- coment/src/App.js
+++ coment/src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import Test from "./user/Test";
 import Coments from "./user/Coment";
 import Form from "./user/Form";
+import { AuthContext } from "./user/auth";
 
 function App() {
   return (
-    <div>
-      <Test />
-      <br />
-      <Coments />
-      <br />
-      <Form />
-    </div>
+    <AuthContext.Provider value={""}>
+      <div>
+        <Test />
+        <br />
+        <Coments />
+        <br />
+        <Form />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
